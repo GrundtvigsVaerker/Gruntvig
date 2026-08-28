e7c797c1ccb471a0b719f0bf4623051d39360f7b
pbvahlst <imvpbv@cc.au.dk>
2026-08-28 09:45:06 +0200

    Improve root assets loading performance for views.
    Remove @before handler that injected all root assets into every single controller method.
    
    Remove sorted version of assets as assets are sorted client-side anynway.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,65 +1,20 @@
 package controllers;
 
 import helpers.Helpers;
+
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+
 import models.Asset;
 import models.Chapter;
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.common.SolrInputDocument;
 import play.mvc.*;
 
-/**
- * Main class, all other controllers should extend this
- */
 public class Application extends Controller {
 
 
-    /**
-     * Make sure header always can reach assets
-     * This way all controllers keeps a sorted version of the assets as arguments
-     * 
-     */
-    @Before
-    static void addAssetToTemplate() {
-        try {
-            renderArgs.put("assets", Asset.find("type = :type order by name").setParameter("type", "root").fetch());
-            List<Asset> sillySortedAssets = Asset.find("type = :type order by name").setParameter("type", "root").fetch();
-            Collections.sort(sillySortedAssets, new SillyComparator());
-            renderArgs.put("sortedAssets", sillySortedAssets);            
-        } catch (Exception e) {
-            renderArgs.put("sortedAssets", Asset.find("type = :type order by name").setParameter("type", "root").fetch());
-            System.out.println("Probably empty db or filenames without year, assets not sorted");
-        }
-    }
-
-        
-    private static String getYearFromFileName(String fileName) {
-        String[] S= fileName.split( "_" );
-        String year= S[0],
-               sj= S[1],
-               litra= "",
-               res;
-        if( sj.matches("[0-9]*[^0-9]+") ) {
-          litra= sj.substring( sj.length()-1 );
-          sj= sj.substring( 0, sj.length()-1 );
-        }
-        res= String.format( "%4s%4s%s", year, sj, litra );
-        //System.out.println( ">"+res );
-        return res;
-    }
-    
-    /**
-     * Sort txt-files in menu by order of year in filename :-)
-     */
-    private static class SillyComparator implements Comparator<Asset> {
-
-        public int compare(Asset t, Asset t1) {
-            return getYearFromFileName(t.fileName).compareTo(getYearFromFileName(t1.fileName));
-        }
-      }
-    
     public static void index() {
         render();
     }
@@ -79,77 +34,119 @@ public class Application extends Controller {
     }
 
     public static void krono() {
+        addAssetToTemplate();
         render();
     }
 
     public static void alfa() {
+        addAssetToTemplate();
         render();
     }
 
     public static void genre() {
+        addAssetToTemplate();
         render();
     }
 
     public static void salmer() {
+        addAssetToTemplate();
         render();
     }
 
-    
+
     // eksempel på en register/leksikon side
     public static void register_side() {
         render();
     }
 
+    static void addAssetToTemplate() {
+        renderArgs.put("rootAssets", Asset.find("type = :type order by name").setParameter("type", "root").fetch());
+        /*
+        try {
+            List<Asset> sillySortedAssets = Asset.find("type = :type order by name").setParameter("type", "root").fetch();
+            Collections.sort(sillySortedAssets, new SillyComparator());
+            renderArgs.put("sortedAssets", sillySortedAssets);
+        } catch (Exception e) {
+            renderArgs.put("sortedAssets", Asset.find("type = :type order by name").setParameter("type", "root").fetch());
+            System.out.println("Probably empty db or filenames without year, assets not sorted");
+        }*/
+    }
 
+        /*
+        private static String getYearFromFileName(String fileName) {
+        String[] S = fileName.split("_");
+        String year = S[0],
+                sj = S[1],
+                litra = "",
+                res;
+        if (sj.matches("[0-9]*[^0-9]+")) {
+            litra = sj.substring(sj.length() - 1);
+            sj = sj.substring(0, sj.length() - 1);
+        }
+        res = String.format("%4s%4s%s", year, sj, litra);
+        //System.out.println( ">"+res );
+        return res;
+    }
+    */
+
+    /**
+     * Sort txt-files in menu by order of year in filename :-)
+     */
+   /* private static class SillyComparator implements Comparator<Asset> {
+
+        public int compare(Asset t, Asset t1) {
+            return getYearFromFileName(t.fileName).compareTo(getYearFromFileName(t1.fileName));
+        }
+    }*/
     public static void testSolr() {
         try {
             SolrServer server = Helpers.getSolrServer();
             SolrInputDocument doc1 = new SolrInputDocument();
-            doc1.addField( "id", "idx" );
-            doc1.addField( "text", "Petters hemmelige tekst" );
-            doc1.addField( "comment", "Petters hemmelige comment" );
-            doc1.addField( "type", "testtype");
+            doc1.addField("id", "idx");
+            doc1.addField("text", "Petters hemmelige tekst");
+            doc1.addField("comment", "Petters hemmelige comment");
+            doc1.addField("type", "testtype");
             server.add(doc1);
             server.commit();
             Application.renderText("Document added: server: " + server);
         } catch (Exception ex) {
             ex.printStackTrace();
-            Application.renderText("Problems with solr, look in log");    
+            Application.renderText("Problems with solr, look in log");
         }
     }
-    
-    
+
+
     // deletes all from solr: run http://localhost:9000/Application/clearSolr
     public static void clearSolr() {
         try {
             SolrServer server = Helpers.getSolrServer();
-            server.deleteByQuery("id:*"); 
+            server.deleteByQuery("id:*");
             server.commit();
             Application.renderText("Solr-data cleared: " + server);
         } catch (Exception ex) {
             ex.printStackTrace();
-            Application.renderText("Problems with solr, look in log");    
-        }        
+            Application.renderText("Problems with solr, look in log");
+        }
     }
-    
-    
+
+
     public static void solrAll() {
         try {
             SolrServer server = Helpers.getSolrServer();
-            server.deleteByQuery("id:*"); 
+            server.deleteByQuery("id:*");
             server.commit();
             List<Asset> all = Asset.findAll();
-            for (Asset a: all) {
+            for (Asset a : all) {
                 a.index();
             }
             List<Chapter> allChapters = Chapter.findAll();
-            for (Chapter c: allChapters) {
+            for (Chapter c : allChapters) {
                 c.index();
             }
             Application.renderText("Done with reindex: " + server);
         } catch (Exception e) {
             e.printStackTrace();
-            Application.renderText("Problems with solr, look in log");                
-        }       
-    }    
+            Application.renderText("Problems with solr, look in log");
+        }
+    }
 }
